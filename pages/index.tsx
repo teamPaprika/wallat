e67b7494e94933d139cfe8b8f959b38fc02e07ad
Youngbum <kimuyb@gmail.com>
2023-02-20 23:46:22 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import "../flow/config";
+import { useState, useEffect } from "react";
+import * as fcl from "@onflow/fcl";
+import Head from "next/head";
+
+const IndexPage = () => {
+  const [user, setUser] = useState({loggedIn: null})
+  const [name, setName] = useState('') // NEW
+
+  useEffect(() => fcl.currentUser.subscribe(setUser), [])
+
+  // NEW
+  const sendQuery = async () => {
+    const profile = await fcl.query({
+      cadence: `
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address)]
+    })
+
+    setName(profile?.name ?? 'No Profile')
+  }
+
+  const AuthedState = () => {
+    return (
+      <div>
+        <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div> {/* NEW */}
+        <button onClick={sendQuery}>Send Query</button> {/* NEW */}
+        <button onClick={fcl.unauthenticate}>Log Out</button>
+      </div>
+    )
+  }
+
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <button onClick={fcl.logIn}>Log In</button>
+        <button onClick={fcl.signUp}>Sign Up</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>FCL Quickstart with NextJS</title>
+        <meta name="description" content="My first web3 app on Flow!" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <h1>Flow App</h1>
+      {user.loggedIn
+        ? <AuthedState />
+        : <UnauthenticatedState />
+      }
+    </div>
+  );
+}
+
+export default IndexPage
