98edbe49cd7e3760cae3f027a496a8e23970f693
ChaeHyunIM <83565916+ChaeHyunIM@users.noreply.github.com>
2023-02-22 00:21:12 +0900

    feed/assets layout initial

--- pages/app/feed.tsx
+++ pages/app/feed.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Box, List, ListItem, ListItemText} from "@mui/material";
 
-
+const FEED_LIST = [
+  {id: 1, title: '설문 1. 가나다라마바사', description: 'EMBLUM is a decentralized protocol for the creation and management of digital assets.'},
+  {id: 1, title: '설문 1. 가나다라마바사', description: 'EMBLUM is a decentralized protocol for the creation and management of digital assets.'},
+  {id: 1, title: '설문 1. 가나다라마바사', description: 'EMBLUM is a decentralized protocol for the creation and management of digital assets.'},
+  {id: 1, title: '설문 1. 가나다라마바사', description: 'EMBLUM is a decentralized protocol for the creation and management of digital assets.'},
+]
 const Feed = () => {
   const a = 1
-  return (<div>Feed</div>)
+  return (<Box sx={{padding: '100px'}}>
+    <Box component={'p'} sx={{fontSize: '24px'}}>Feed</Box>
+    <List sx={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+      {FEED_LIST.map((item) => {
+        return (<ListItem sx={{width: '70vw', border: '1px solid black', backgroundColor: '#E6E6E6'}}>
+          <ListItemText primary={item.title}></ListItemText>
+        </ListItem>)
+      })}
+    </List>
+  </Box>)
 }
 
 export default Feed;
